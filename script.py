7e49734c5b5b900b547a85506c5136e48e6d2a3c
Bastian Lipka <68615019+kirbs-btw@users.noreply.github.com>
2023-03-21 20:25:15 +0100

    Add files via upload

--- script.py
+++ script.py
@@ -3,7 +3,7 @@ from stl import mesh
 from mpl_toolkits import mplot3d
 from matplotlib import pyplot
 
-cube = mesh.Mesh.from_file('C:/Users/Bastian/Desktop/stl_test/cube.stl')
+cube = mesh.Mesh.from_file('C:/Users/Bastian/Desktop/stl_test/plane.stl')
 """
 VERTICE_COUNT = 8
 data = np.zeros(VERTICE_COUNT, dtype=mesh.Mesh.dtype)
@@ -14,14 +14,24 @@ fig = pyplot.figure()
 
 ax = fig.add_subplot(projection='3d')
 
-print("the vectors are: \n")
-for i in np.unique(cube.vectors, axis=0):    
+points = np.around(np.unique(cube.vectors.reshape([int(cube.vectors.size/3), 3]), axis=0), 2)
+
+# change point depending on the distance to the center point
+
+for i in points:    
     print(i)
     ax.scatter(i[0], i[1], i[2])
 
+"""
+for i in np.unique(cube.vectors, axis=0):    
+    print(i)
+    ax.scatter(i[0], i[1], i[2])
+"""
+"""
 for i in np.unique(cube.normals, axis=0):    
     print(i)
     ax.scatter(i[0], i[1], i[2])
+"""
 
 pyplot.show()
 
