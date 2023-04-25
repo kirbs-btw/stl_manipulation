c215ee8654bd52b4fcdd2951f38c608283d07512
Bastian Lipka <68615019+kirbs-btw@users.noreply.github.com>
2023-04-25 10:27:07 +0200

    Add files via upload

---
+++ alternate.py
@@ -0,0 +1,41 @@
+import numpy as np
+from stl import mesh
+from mpl_toolkits import mplot3d
+from matplotlib import pyplot
+import math
+import random as rand
+
+def curve_points(points) -> list:
+    for point in points:
+        dist =  math.sqrt(point[0]**2 + point[1]**2 + point[2]**2)
+    
+        fac = rand.randint(0, 10)
+        print(fac)
+
+        point[2] = fac
+
+    return points
+
+def main() -> None:
+    plane = mesh.Mesh.from_file('H:/Projekte/Projekte/Project 137/stl_test/plane.stl')
+
+    # define graph
+    fig = pyplot.figure()
+    ax = fig.add_subplot(projection='3d')
+
+    # get points
+    points = np.around(np.unique(plane.vectors.reshape([int(plane.vectors.size/3), 3]), axis=0), 2)
+    curve_points(points)
+
+
+    # scatter points on graph
+    for i in points:    
+        print(i)
+        ax.scatter(i[0], i[1], i[2])
+
+    # shows the final graph
+    pyplot.show()
+
+
+if __name__ == '__main__':
+    main()
